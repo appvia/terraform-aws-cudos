f5c8ca186191ba57da35eae299ef2c6c3a68e7ba
Rohith Jayawardene <gambol99@gmail.com>
2024-11-18 14:48:27 +0000

    chore: adding the changes

--- modules/destination/data.tf
+++ modules/destination/data.tf
@@ -1,3 +1,6 @@
 
 ## Find the current identity for the cost analysis session 
 data "aws_caller_identity" "current" {}
+
+## Find the current region 
+data "aws_region" "current" {}
--- modules/destination/outputs.tf
+++ modules/destination/outputs.tf
@@ -1,4 +1,9 @@
 
+output "cloudformation_bucket_arn" {
+  description = "The name of the bucket where to store the CloudFormation"
+  value       = module.cloudformation.s3_bucket_arn
+}
+
 output "destination_bucket_arn" {
   description = "The name of the bucket where to replicate the data from the CUR"
   value       = module.collector.cur_bucket_arn
@@ -9,3 +14,7 @@ output "destination_bucket_name" {
   value       = module.collector.cur_bucket_name
 }
 
+output "dashboard_bucket_arn" {
+  description = "The name of the bucket where to store the dashboards"
+  value       = module.dashboard_bucket.s3_bucket_arn
+}
