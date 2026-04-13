0acd32ea2d56ae5c4df99896b54c9445fdb0c224
Rohith Jayawardene <gambol99@gmail.com>
2026-04-13 22:34:32 +0100

    fix: removed the aliases from the example, as they are no longer used

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -5,16 +5,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "cost_analysis_us_east_1"
-  region = "us-east-1"
-}
-
-provider "aws" {
   alias  = "management"
   region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "management_us_east_1"
-  region = "us-east-1"
 }
